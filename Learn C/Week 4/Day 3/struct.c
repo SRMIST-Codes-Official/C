705e5e041f332cf0e6bde6e1d4d671c38ef44c76
Jayesh Purswani <jppurswani2004@gmail.com>
2024-09-15 22:56:42 +0530

    bugs removed

--- Learn C/Week 4/Day 3/struct.c
+++ Learn C/Week 4/Day 3/struct.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
+#include<string.h>  // Include for strcpy()
 
 struct Student
 {
     char name[25];
     int age;
     char branch[10];
-    //F for female and M for male
+    // F for female and M for male
     char gender;
 };
 
@@ -13,21 +14,18 @@ int main()
 {
     struct Student s1;
     
- // s1 is a variable of Student type and 
-// age,name,branch,gender is a member of StudeNT and assigning values to them  using // dot(.)operator
-        
+    // Assigning values to the members of s1 using dot(.) operator
     s1.age = 18;
-    s1.name = "John";
-    s1.branch = "C.S.E";
-    s1.gender = "F";
+    strcpy(s1.name, "John");
+    strcpy(s1.branch, "C.S.E");  
     
     /*
-        displaying the stored values
+        Displaying the stored values
     */
     printf("Name of Student 1: %s\n", s1.name);
     printf("Age of Student 1: %d\n", s1.age);
     printf("Branch of Student 1: %s\n", s1.branch);
-    printf("Gender of Student 1: %s\n", s1.gender);
+    printf("Gender of Student 1: %c\n", s1.gender);
     
     return 0;
 }
